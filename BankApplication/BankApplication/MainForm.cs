d92f1c3c53571ee50769b154cda9466141c2e1f4
ArthurHoang15 <arthurhoangcode@gmail.com>
2025-05-16 15:07:17 +0700

    Add Front-end

---
+++ BankApplication/BankApplication/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankApplication
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+            var customerForm = new CustomerForm();
+            customerForm.ShowDialog();
+        }
+
+        private void btnAccounts_Click(object sender, EventArgs e)
+        {
+            var accountForm = new SavingAccountForm();
+            accountForm.ShowDialog();
+        }
+
+        private void btnTransactions_Click(object sender, EventArgs e)
+        {
+            var transactionForm = new TransactionForm();
+            transactionForm.ShowDialog();
+        }
+    }
+}
