0a6e89755229fd8e2974b37f41e44c96181c08c6
Tad Yuh <tadyuh76@gmail.com>
2025-05-16 15:12:27 +0700

    complete backend

--- BankSystem/Client/AccountService.cs
+++ BankSystem/Client/AccountService.cs
@@ -12,9 +12,66 @@ namespace Client
             _context = new AppDbContext();
         }
 
-        //public BankAccount Authenticate(long accountId, string password)
-        //{
+        public BankAccount Authenticate(long accountId, string password)
+        {
+            return _context.BankAccounts
+                .FirstOrDefault(b => b.AccountId == accountId && b.Password == password);
+        }
 
-        //}
+        public BankAccount GetAccountInfo(long accountId)
+        {
+            return _context.BankAccounts
+                .FirstOrDefault(b => b.AccountId == accountId);
+        }
+
+        public List<Transaction> GetTransactions(long accountId)
+        {
+            return _context.Transactions
+                .Where(t => t.AccountId == accountId)
+                .OrderByDescending(t => t.HappenedTime)
+                .ToList();
+        }
+
+        public bool Deposit(long accountId, double amount)
+        {
+            if (amount <= 0)
+            {
+                return false;
+            }
+
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var account = _context.BankAccounts
+                        .FirstOrDefault(b => b.AccountId == accountId);
+                    if (account == null)
+                    {
+                        return false;
+                    }
+
+                    account.Balance += amount;
+
+                    _context.Transactions.Add(new Transaction
+                    {
+                        TransId = Guid.NewGuid(),
+                        AccountId = accountId,
+                        HappenedTime = DateTime.Now,
+                        ActionDesc = "Deposit",
+                        Note = $"Deposited {amount} to account {accountId}"
+                    });
+                    _context.SaveChanges();
+
+                    transaction.Commit();
+                    return true;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return false;
+                }
+            }
+        }
+
     }
 }
---
+++ BankSystem/Data/Migrations/20250516075659_BankSystem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BankSystem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    AccountId = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OwnerName = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerPhone = table.Column<string>(type: "TEXT", nullable: false),
+                    Balance = table.Column<double>(type: "REAL", nullable: false),
+                    AccountType = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.AccountId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AccountId = table.Column<long>(type: "INTEGER", nullable: false),
+                    HappenedTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ActionDesc = table.Column<string>(type: "TEXT", nullable: false),
+                    Note = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransId);
+                    table.ForeignKey(
+                        name: "FK_Transactions_BankAccounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "AccountId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "BankAccounts",
+                columns: new[] { "AccountId", "AccountType", "Balance", "OwnerAddress", "OwnerName", "OwnerPhone", "Password" },
+                values: new object[,]
+                {
+                    { 1L, "Checking", 1000.0, "17 Nguyen Tri Phuong St.", "Huy Huong", "0914398183", "123456" },
+                    { 2L, "Checking", 2000.0, "18 Nguyen Tri Phuong St.", "Quan Hoang", "0914398184", "123456" },
+                    { 3L, "Checking", 3000.0, "19 Nguyen Tri Phuong St.", "Quang Tran", "0914398185", "123456" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_AccountId",
+                table: "Transactions",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+        }
+    }
+}
